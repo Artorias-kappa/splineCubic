539c9ccf786b33fc1cef80f8373cfc5e588305f7
Exa <sofiane.couedel@gmail.com>
2020-05-20 16:47:40 +0200

    matrice tridaginial main

--- src/main/main.java
+++ src/main/main.java
@@ -1,19 +1,60 @@
 package main;
 
+import java.util.ArrayList;
+
 public class main {
 
 
 
+
 	public static void main(String[] args) {
+		
+		int sizeTest = 4;
+		
+		ArrayList<Double> test = initTab(sizeTest);
 
-		float tabX[] = {};
-		float tabY[] = {};
+		System.out.println(display(test, sizeTest));
 
-		float tabA[] = {};
 
 
 
 
 	}
 
+	public static ArrayList<Double> initTab(int size) {
+		ArrayList<Double> newList = new ArrayList<Double>();
+
+		for (int i = 0 ; i < size ; ++i) {
+			for (int j = 0 ; j < size ; ++j) {
+				if (i == j) {
+					newList.add(i * size + j, 4.0);
+				} else if (i*i +1 == j) {
+					newList.add(i * size + j, 1.0);
+				} else if (i*i - 1 == j) {
+					newList.add(i * size + j, 1.0);
+				} else {
+					newList.add(i * size + j, 0.0);
+				}
+			}
+		}
+		return newList;
+	}
+
+	public static String display(ArrayList<Double> list, int size) {
+
+		String display = "";
+
+		for (int i = 0 ; i < list.size() ; ++i) {
+			for (int j = 0 ; j < list.size() ; ++j) {
+				display += list.get(i * 4 + j);
+			}
+		}
+		return display;
+	}
+
+
+
 }
+
+
+
