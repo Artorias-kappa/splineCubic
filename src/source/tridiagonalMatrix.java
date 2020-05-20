d5e600d5f85a9c714ed48f5c5dcb1d0582e9d6b5
Salade <cyprien.garnier@orange.fr>
2020-05-20 18:23:10 +0200

    ArrayList -> tableau

--- src/source/tridiagonalMatrix.java
+++ src/source/tridiagonalMatrix.java
@@ -5,10 +5,10 @@ import java.util.ArrayList;
 public class tridiagonalMatrix {
 	private int lenght;
 	
-	private ArrayList<Double> matrix;
+	private double matrix[];
 	
 	
-	public tridiagonalMatrix(int lenght, ArrayList<Double> matrix) {
+	public tridiagonalMatrix(int lenght, double matrix[]) {
 		
 		this.lenght = lenght;
 		this.matrix = matrix;
@@ -36,11 +36,11 @@ public class tridiagonalMatrix {
 		double value;
 	    // we start with the last pivot (A[(n-1) * n + (n-1)]) and go up...
 	    for(int i = A.lenght - 1 ; i >= 0 ; i--) {
-	        y.tabElement.set(i, x.tabElement.get(i) / A.matrix.get(i * A.lenght + i)); // Look for the result of the only variable in the last row of A, such that t = ...
+	        y.tabElement[i] =  x.tabElement[i] / A.matrix[i * A.lenght + i]; // Look for the result of the only variable in the last row of A, such that t = ...
 
 	        for (int j = i - 1; j >= 0; j--) {
-	            value = x.tabElement.get(i) * A.matrix.get(j * A.lenght + i); // ... then we replace (here t) by its value found previously, on the column ...
-	            x.tabElement.set(j, x.tabElement.get(j) - value); // ... and we move this value on the side of b to leave only the variables in the A matrix.
+	            value = x.tabElement[i] * A.matrix[j * A.lenght + i]; // ... then we replace (here t) by its value found previously, on the column ...
+	            x.tabElement[j] -= value; // ... and we move this value on the side of b to leave only the variables in the A matrix.
 	        }
 	    }
 		
@@ -49,14 +49,14 @@ public class tridiagonalMatrix {
 
 	private void solveTriangularSystemDown(vector x, tridiagonalMatrix A, vector b) {
 		double value;
-	    x.tabElement.set(0, b.tabElement.get(0));
+	    x.tabElement[0] = b.tabElement[0];
 
 	    for(int i = 1 ; i <= A.lenght ; i++) {
-	        value = b.tabElement.get(i);
+	        value = b.tabElement[i];
 	        for (int j = 0; j <= i; j++) {
-	            value -= A.matrix.get(i*A.lenght+j)*x.tabElement.get(j);
+	            value -= A.matrix[i*A.lenght+j]*x.tabElement[j];
 	        }
-	        x.tabElement.set(i, value);
+	        x.tabElement[i] = value;
 	   }
 	
 	}
@@ -68,16 +68,16 @@ public class tridiagonalMatrix {
 		for (int cl = 0; cl < A.lenght - 1; cl++) {
 	        for (int lgn = cl + 1; lgn < A.lenght; lgn++) {
 	            // if the coef is not null
-	            if (A.matrix.get(cl * A.lenght + cl) != 0) {
+	            if (A.matrix[cl * A.lenght + cl] != 0) {
 	                // determine the coef x (exp L2 = L2 - xL1)
-	                 coef = A.matrix.get(lgn * A.lenght + cl) / A.matrix.get(cl * A.lenght + cl);
+	                 coef = A.matrix[lgn * A.lenght + cl] / A.matrix[cl * A.lenght + cl];
 	                // performs the calculation (L2 = L2-xL1) by applying x to each cell of the line
 
 	                for (int w = cl; w < A.lenght; w++) {
-	                    A.matrix.set(lgn * A.lenght + w, A.matrix.get(lgn * A.lenght + w) - coef * A.matrix.get(cl * A.lenght + w));
+	                    A.matrix[lgn * A.lenght + w] = A.matrix[lgn * A.lenght + w] - coef * A.matrix[cl * A.lenght + w];
 	                }
 
-	                A.matrix.set(lgn * A.lenght + cl, coef);
+	                A.matrix[lgn * A.lenght + cl] =  coef;
 	            }
 	        }
 	    }
