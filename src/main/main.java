98e170111e6e4a46b8ee8f89b96767f8ccbf29b0
Salade <cyprien.garnier@orange.fr>
2020-05-20 17:49:38 +0200

    test

--- src/main/main.java
+++ src/main/main.java
@@ -5,7 +5,17 @@ public class main {
 
 
 	public static void main(String[] args) {
-
+		
+		int sizeTest = 4;
+		
+		double test[] = {
+				4, 1, 0, 0,
+				1, 4, 1, 0,
+				0, 1, 4, 1,
+				0, 0, 1, 4
+		};
+		
+		
 		float tabX[] = {};
 		float tabY[] = {};
 
